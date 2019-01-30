f7d9d0ab2ff7de6dadcb020bfded7928dcc6dc6f
Miriam Wagner <miriam.wagner@wagner-dn.de>
2019-01-30 13:22:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function takeANumber (line,person) {
   var waitingposition = line.length + 1
+  line.push(person)
   return "Welcome, "+ person + ". You are number " + waitingposition + " in line."
 }
